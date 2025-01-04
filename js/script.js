e53253a9ae666500a65c2d59a76a6ff953009eab
mohammedanaf <mohammedanaf.developer@gmail.com>
2025-01-04 18:49:49 +0530

    PWA-enabled

--- js/script.js
+++ js/script.js
@@ -94,3 +94,17 @@ const filter = function () {
 }
 
 addEventonElements(filterBtn, "click", filter)
+
+// Registering service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        })
+        .catch((error) => {
+          console.log('ServiceWorker registration failed: ', error);
+        });
+    });
+  }
+  
